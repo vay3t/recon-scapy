55ee7481f043a716d7b54d85da97ba5090ee8cf0
Vay3t <fffyanez@gmail.com>
2018-09-20 09:17:25 -0300

    Update recon-scapy.py
    added more functions

--- recon-scapy.py
+++ recon-scapy.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python2
 # Coded by vay3t!
 
+from scapy.all import *
+from datetime import datetime
+import IPy
+import sys
 import commands
 import os
-import sys
 
-from scapy.all import *
-import IPy
 
 # rdpcap comes from scapy and loads in our pcap file
 #packets = rdpcap(sys.argv[1])
@@ -27,17 +28,21 @@ def detect_netmask(iface):
 	netmask = commands.getoutput('ifconfig '+iface+' | grep "inet "').split()[3]
 	return netmask
 
-def detect_network(iface):
+def detect_network(iface,netmask):
 	from netaddr import IPAddress
 	bitNetmask = IPAddress(netmask).netmask_bits()
 	network = commands.getoutput("route -n | grep "+iface+" | grep '"+netmask+"' | awk '{print $1}'")
 	return network+"/"+str(bitNetmask)
 
+
 iface = detect_iface()
 myip = detect_ip(iface)
+
 gateway = detect_gateway()
+
 netmask = detect_netmask(iface)
-network = detect_network(iface)
+network = detect_network(iface,netmask)
+
 
 
 def dns_dump(packet):
@@ -89,16 +94,88 @@ def arp_recon(packet):
 			hosts.add(complete_string)
 			print complete_string
 
+def arp_display(packet):
+    if packet[ARP].op == 1: #who-has (request)
+        print '[->] Request: {} is asking about {}'.format(packet[ARP].psrc, packet[ARP].pdst)
+    if packet[ARP].op == 2: #is-at (response)
+        print '[<-] Response: {} has address {}'.format(packet[ARP].hwsrc, packet[ARP].psrc)
+
+def mac_recon(packet):
+	if packet.haslayer(IP):
+		ipSrc = packet['IP'].src
+		type_ipSrc = IPy.IP(ipSrc)
+		macSrc = packet['Ether'].src
+
+		ipDst = packet['IP'].dst
+		type_ipDst = IPy.IP(ipDst)
+		macDst = packet['Ether'].dst
+
+		if type_ipSrc.iptype() == 'PRIVATE':
+			if macSrc != "ff:ff:ff:ff:ff:ff" or macGateway:
+				if ipSrc not in hosts:
+					complete_string = macDst + " - " + ipDst
+					hosts.add(complete_string)
+					print(complete_string)
+		if type_ipDst.iptype() == 'PRIVATE':
+			if macDst != "ff:ff:ff:ff:ff:ff" or macGateway:
+				if ipDst not in hosts:
+					complete_string = macDst + " - " + ipDst
+					hosts.add(complete_string)
+					print(complete_string)
+
+# logs
+def datenow():
+    x = datetime.now()
+    return x.strftime("[ %d/%m/%Y %H:%M:%S ] ")
+
+
+# Poison detect
+def check_spoof(source, mac, destination):
+    # Function checks if a specific ARP reply is part of an ARP spoof attack or not
+    if destination == broadcast:
+        if not mac in replies_count:
+            replies_count[mac] = 0
+    if not source in requests and source != local_ip:
+        if not mac in replies_count:
+            replies_count[mac] = 0
+        else:
+            replies_count[mac] += 1
+        # Logs ARP Reply
+        print("{} ARP replies detected from MAC {}. Request count {}".format(datenow(),mac, replies_count[mac]))
+        if (replies_count[mac] > request_threshold) and (not mac in notification_issued):
+            # Check number of replies reaches threshold or not, and whether or not we have sent a notification for this MAC addr
+            print("{} ARP Spoofing Detected from MAC Address {}".format(datenow(),mac)) # Logs the attack in the log file
+            # Issue OS Notification
+            print("[!] ARP Spoofing Detected -> {}".format(mac))
+            # Add to sent list to prevent repeated notifications.
+            notification_issued.append(mac)
+    else:
+        if source in requests:
+            requests.remove(source)
+
+def poison_detect(packet):
+    # Retrieve necessary parameters from packet
+    source = packet.sprintf("%ARP.psrc%")
+    dest = packet.sprintf("%ARP.pdst%")
+    source_mac = packet.sprintf("%ARP.hwsrc%")
+    operation = packet.sprintf("%ARP.op%")
+    if source == local_ip:
+        requests.append(dest)
+    if operation == 'is-at':
+        return check_spoof(source, source_mac, dest)
 
 def help():
-	print """usage: python2 """ + sys.argv[0] + """ <option>
+	print """usage: """ + sys.argv[0] + """ <option>
 
 	help - Show help
 	arping - Discovery hosts with ARP
 	arprecon - Discovery host with passive sniffing ARP
+	arpdisplay - View ARP requests and responses
 	recon - Discovery hosts with passive sniffing
 	onlydns - Collect DNS with passive sniffing
 	dnsdump - View DNS requests of hosts with passive sniffing
+	macrecon - Recon hosts with MACs without ARP
+	posiondetect - Detect ARP Poison
 """
 
 if os.geteuid() != 0:
@@ -110,12 +187,25 @@ elif sys.argv[1] == "help":
 elif sys.argv[1] == "arping":
 	arping_scan(network)
 elif sys.argv[1] == "arprecon":
-	sniff(iface=iface,prn=arp_recon)
+	sniff(iface=iface,filter="arp",prn=arp_recon)
 elif sys.argv[1] == "onlydns":
-	sniff(iface=iface,prn=dns_sniff)
+	sniff(iface=iface,filter="udp port 53 and not host "+myip,prn=dns_sniff)
 elif sys.argv[1] == "dnsdump":
-	sniff(iface=iface,prn=dns_dump)
+	sniff(iface=iface,filter="udp port 53 and not host "+myip,prn=dns_dump)
 elif sys.argv[1] == "recon":
-	sniff(iface=iface,prn=ip_dump_priv)
+	sniff(iface=iface,filter="not host "+myip,prn=ip_dump_priv)
+elif sys.argv[1] == "arpdisplay":
+	sniff(iface=iface,filter="arp and not host "+myip,prn=arp_display,)
+elif sys.argv[1] == "macrecon":
+	macGateway = getmacbyip(gateway)
+	hosts.add(macGateway+" - "+gateway)
+	sniff(iface=iface,filter="not host "+myip,prn=arp_recon)
+elif sys.argv[1] == "poisondetect":
+	request_threshold = 10
+	requests = []
+	replies_count = {}
+	notification_issued = []
+	print datenow()+"ARP Spoofing Detection Started on "+network
+	sniff(iface=iface,filter="arp", prn=poison_detect, store=0)
 else:
 	help()
